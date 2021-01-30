eeb8f619ec72812d11235bec9f497d53b9057e06
Arjen de Aldrey <adealdrey3@gmail.com>
2021-01-30 01:15:49 +0100

    [ADD] Panic System, using vignette, color adjust, bloom

---
+++ Project Lost and Found/Assets/Scripts/PanicSystem.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(Volume))]
+public class PanicSystem : MonoBehaviour
+{
+    private StressData playerStress;
+
+    private Volume stack;
+    private Vignette pp_vignette;
+    private ColorAdjustments pp_color;
+    private Bloom pp_bloom;
+
+    private float vignette_intensity_max;
+    [SerializeField] private float vignette_activation_speed;
+
+    private Color color_max;
+    [SerializeField] private float color_activation_speed;
+
+    private float bloom_intensity_max;
+    [SerializeField] private float bloom_activation_speed;
+
+    [SerializeField] [Range(0, 100)] private int vignetteActivation; 
+    [SerializeField] [Range(0, 100)] private int colorActivation; 
+    [SerializeField] [Range(0, 100)] private int bloomActivation; 
+
+    void Start()
+    {
+        playerStress = FindObjectOfType<StressData>();
+        stack = GetComponent<Volume>();
+        stack.profile.TryGet(out pp_vignette);
+        stack.profile.TryGet(out pp_color);
+        stack.profile.TryGet(out pp_bloom);
+
+        vignette_intensity_max = pp_vignette.intensity.value;
+        color_max = pp_color.colorFilter.value;
+        bloom_intensity_max = pp_bloom.intensity.value;
+
+        pp_vignette.intensity.value = 0.0f;
+        pp_color.colorFilter.value = Color.white;
+        pp_bloom.intensity.value = 0.0f;
+    }
+
+    void Update()
+    {
+        if(playerStress != null)
+        {
+            float stress = playerStress.GetStress();
+            if (stress >= vignetteActivation)
+                HandleVignette(false);
+            else
+                HandleVignette(true);
+
+            if (stress >= colorActivation)
+                HandleColor(false);
+            else
+                HandleColor(true);
+
+            if (stress >= bloomActivation)
+                HandleBloom(false);
+            else
+                HandleBloom(true);
+        }
+    }
+
+    private void HandleVignette(bool down)
+    {
+        if (pp_color.colorFilter.value != color_max && !down)
+            pp_color.colorFilter.value = Color.Lerp(pp_color.colorFilter.value, color_max, color_activation_speed);
+        else if(pp_color.colorFilter.value != Color.white)
+            pp_color.colorFilter.value = Color.Lerp(pp_color.colorFilter.value, Color.white, color_activation_speed);
+    }
+    private void HandleColor(bool down)
+    {
+        if (pp_vignette.intensity.value != vignette_intensity_max && !down)
+            pp_vignette.intensity.value = Mathf.Lerp(pp_vignette.intensity.value, vignette_intensity_max, vignette_activation_speed);
+        else if (pp_vignette.intensity.value > 0.0001f)
+            pp_vignette.intensity.value = Mathf.Lerp(pp_vignette.intensity.value, 0.0f, vignette_activation_speed);
+    }
+    private void HandleBloom(bool down)
+    {
+        if (pp_bloom.intensity.value != bloom_intensity_max && !down)
+            pp_bloom.intensity.value = Mathf.Lerp(pp_bloom.intensity.value, bloom_intensity_max, bloom_activation_speed);
+        else if (pp_bloom.intensity.value > 0.0001f)
+            pp_bloom.intensity.value = Mathf.Lerp(pp_bloom.intensity.value, 0.0f, bloom_activation_speed);
+    }
+}
--- Project Lost and Found/Assets/Scripts/StressData.cs
+++ Project Lost and Found/Assets/Scripts/StressData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class StressData : MonoBehaviour
 {
-    private float stressLevel = 0.0f;
+    [SerializeField] [Range(0.0f,100.0f)] private float stressLevel = 0.0f;
     public void AddToStress(float addition) => stressLevel += addition;
     public void RemoveFromStress(float addition) => stressLevel -= addition;
     public float GetStress() => stressLevel;
